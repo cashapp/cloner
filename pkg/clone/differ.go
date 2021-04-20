2dd6bf2f7bb907163f1cdcac3abf7540b1db2fbd
Jon Tirsen <jontirsen@squareup.com>
2021-04-20 13:30:34 +0200

    Close result set to return connection to pool

--- pkg/clone/differ.go
+++ pkg/clone/differ.go
@@ -404,6 +404,7 @@ func checksumChunk(ctx context.Context, config ReaderConfig, from string, reader
 		sql := fmt.Sprintf("SELECT %s BIT_XOR(%s) FROM `%s` %s",
 			hint, strings.Join(chunk.Table.CRC32Columns, " ^ "), chunk.Table.Name, chunkWhere(chunk, extraWhereClause))
 		rows, err := reader.QueryContext(ctx, sql)
+		defer rows.Close()
 		if err != nil {
 			return errors.WithStack(err)
 		}

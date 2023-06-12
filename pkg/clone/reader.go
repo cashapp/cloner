5f068fb403afdd84fce03846397153621eb66e3c
Jon Tirsen <jontirsen@squareup.com>
2023-06-12 18:18:49 +0200

    Lock tables after all retries

--- pkg/clone/reader.go
+++ pkg/clone/reader.go
@@ -3,8 +3,11 @@ package clone
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"math/rand"
+	"time"
 
+	"github.com/cenkalti/backoff/v4"
 	"github.com/pkg/errors"
 	"github.com/platinummonkey/go-concurrency-limits/core"
 	"github.com/prometheus/client_golang/prometheus"
@@ -148,6 +151,90 @@ func (r *Reader) processChunk(ctx context.Context, diffsCh chan Diff, diff bool,
 		}
 	}
 
+	return nil
+}
+
+func (r *Reader) diffChunkWithTableLock(ctx context.Context, chunk Chunk) (diffs []Diff, err error) {
+	log.Infof("diffing chunk %v with table lock, max duration %v", chunk.String(), r.config.TableLockMaxDuration)
+
+	startTime := time.Now()
+	lockConn, err := r.lockTable(ctx)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer func(r *Reader, ctx context.Context, lockConn *sql.Conn) {
+		err := r.unlockTables(ctx, lockConn)
+		if err != nil {
+			log.WithError(err).Warnf("failed to unlock table %v: %v", r.table.Name, err)
+			_ = lockConn.Close()
+		}
+	}(r, ctx, lockConn)
+	// Other connections can still read the table (just not write), so we don't have to use the lockConn to do the diff
+
+	tries := 0
+	timeoutCtx, cancelFunc := context.WithTimeout(ctx, r.config.TableLockMaxDuration)
+	defer cancelFunc()
+	exponentialBackOff := backoff.NewExponentialBackOff()
+	exponentialBackOff.InitialInterval = time.Millisecond
+	exponentialBackOff.MaxInterval = 3 * time.Second
+	exponentialBackOff.MaxElapsedTime = r.config.TableLockMaxDuration
+	backOff := backoff.WithContext(
+		backoff.WithMaxRetries(
+			exponentialBackOff, uint64(r.config.FailedChunkRetryCount)),
+		timeoutCtx)
+	err = backoff.Retry(func() error {
+		diffs = nil
+		diffs, err = r.doDiffChunk(ctx, chunk)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		if len(diffs) == 0 {
+			if tries > 1 {
+				log.Infof("chunk %s[%v-%v) had no diffs after %d retries",
+					chunk.Table.Name, chunk.Start, chunk.End, tries)
+			}
+			// Yay! Chunk had no diffs!!
+			return nil
+		} else {
+			if r.config.FailedChunkRetryCount-tries > 0 {
+				log.Infof("chunk %s[%d-%d) had diffs, retrying %d more times",
+					chunk.Table.Name, chunk.Start, chunk.End, r.config.FailedChunkRetryCount-tries)
+				tries++
+			}
+			return &ChunkRetryError{Chunk: chunk}
+		}
+	}, backOff)
+	if errors.Is(err, &ChunkRetryError{}) {
+		err = nil
+	}
+	lockDuration := time.Now().Sub(startTime)
+	if len(diffs) > 0 {
+		log.Infof("%d diffs found still in chunk %v after diffing with table lock, total lock duration: %v", len(diffs), chunk.String(), lockDuration)
+	} else {
+		log.Infof("no diffs found in chunk %v after diffing with table lock, total lock duration: %v", chunk.String(), lockDuration)
+	}
+	return diffs, nil
+}
+
+func (r *Reader) lockTable(ctx context.Context) (*sql.Conn, error) {
+	log.Infof("locking table for reads only: %v", r.table.Name)
+	conn, err := r.source.Conn(ctx)
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("LOCK TABLE `%s` READ", r.table.Name))
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return conn, err
+}
+
+// unlockTables unlocks all tables and closes the connection
+func (r *Reader) unlockTables(ctx context.Context, conn *sql.Conn) error {
+	log.Infof("unlocking table: %v", r.table.Name)
+	defer conn.Close()
+	conn, err := r.source.Conn(ctx)
+	_, err = conn.ExecContext(ctx, "UNLOCK TABLES")
+	if err != nil {
+		return errors.WithStack(err)
+	}
 	return nil
 }
 
